0203ef8a4e60bee26e181d94a299aa954bf1deff
Jakewein25 <Jweinstein@albany.edu>
2025-03-24 20:10:56 -0400

    Add files via upload
    March 24: classwork

---
+++ output.js
@@ -0,0 +1,15 @@
+function showInnerHTML() {
+ document.getElementById("output1").innerHTML = "Hello from innerHTML!";
+}
+
+function useDocumentWrite() {
+ document.write("I went to Florida over break.");
+}
+
+function showAlert() {
+ alert("I tanned alot.");
+}
+
+function showConsole() {
+ console.log("I went to the gym.");
+}
---
+++ variables.js
@@ -0,0 +1,15 @@
+// Using let
+let name = "Jake";
+let age = "20";
+let isStudent = true;
+
+document.getElementById("demo1").innerHTML = 
+ "Name: " + name + "<br>Age: " + age + "<br>Student: " + isStudent;
+
+var city = "Albany";   //Function-scoped
+let state = "NY";      //Block-scoped
+const country = "USA"; //Cannot be reassigned
+
+document.getElementById("demo2").innerHTML =
+  "Location: " + city + ", " + state + ", " + country;
+
